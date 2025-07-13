dfc5b608f0a7c22adef223417f479bee55a17924
medkinson <medkinson@mishland.ru>
2025-07-13 22:13:44 +0300

    add admin_check utility (can_restrict_check)

---
+++ bot/utils/admin_check.py
@@ -0,0 +1,18 @@
+from aiogram import Bot
+from aiogram.types import ChatMemberAdministrator, ChatMemberOwner
+from functools import wraps
+from aiogram.types import Message
+
+def can_restrict_check():
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(message: Message):
+            bot = message.bot
+
+            member = await bot.get_chat_member(message.chat.id, message.from_user.id)
+            if (isinstance(member, ChatMemberAdministrator) and member.can_restrict_members) or (isinstance(member, ChatMemberOwner)):
+                return await func(message)
+            else:
+                await message.reply("govno nishe")
+        return wrapper
+    return decorator
