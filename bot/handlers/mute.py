1def83a39b1e1e44662735913fc801ddeb26ef75
medkinson <medkinson@mishland.ru>
2025-07-15 23:10:35 +0300

    refactor can_restrict_check decorator

--- bot/handlers/mute.py
+++ bot/handlers/mute.py
@@ -7,7 +7,7 @@ from bot.utils.admin_check import can_restrict_check
 router = Router()
 
 @router.message(Command("mute"))
-@can_restrict_check()
+@can_restrict_check
 async def handle_mute(message: Message) -> None:
     await message.bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id, permissions=ChatPermissions(can_send_messages=False))
     await message.answer(f"Successfully muted {message.reply_to_message.from_user.first_name} by admin {message.from_user.first_name}!")
--- bot/utils/admin_check.py
+++ bot/utils/admin_check.py
@@ -3,16 +3,14 @@ from aiogram.types import ChatMemberAdministrator, ChatMemberOwner
 from functools import wraps
 from aiogram.types import Message
 
-def can_restrict_check():
-    def decorator(func):
-        @wraps(func)
-        async def wrapper(message: Message):
-            bot = message.bot
 
-            member = await bot.get_chat_member(message.chat.id, message.from_user.id)
-            if (isinstance(member, ChatMemberAdministrator) and member.can_restrict_members) or (isinstance(member, ChatMemberOwner)):
-                return await func(message)
-            else:
-                await message.reply("govno, get admin perms first")
-        return wrapper
-    return decorator
+def can_restrict_check(func):
+    async def wrapper(message: Message):
+        bot = message.bot
+
+        member = await bot.get_chat_member(message.chat.id, message.from_user.id)
+        if (isinstance(member, ChatMemberAdministrator) and member.can_restrict_members) or (isinstance(member, ChatMemberOwner)):
+            return await func(message)
+        else:
+            await message.reply("govno, get admin perms first")
+    return wrapper
